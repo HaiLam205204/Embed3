8df0c126e17e99b94be79f442f0d6dc31bb56ca5
Vally <113294564+Needalife@users.noreply.github.com>
2025-05-16 23:26:06 +0700

    add import

---
+++ src/game/combat_interface.c
@@ -0,0 +1,8 @@
+#include "../../include/combat_interface.h"
+#include "../../include/framebf.h"
+#include "../../include/uart0.h"
+
+#define HP_BAR_WIDTH 50
+#define HP_BAR_HEIGHT 5
+#define RED 0xFFFF0000
+#define GREEN 0xFF00FF00
