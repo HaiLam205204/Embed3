3cc3c44bc43374efe9e373f1f6b3961d10e84eb5
Vally <113294564+Needalife@users.noreply.github.com>
2025-05-16 23:27:08 +0700

    Update game_design.c

--- src/game/game_design.c
+++ src/game/game_design.c
@@ -3,11 +3,9 @@
 #include "../../include/game_design.h"
 #include "../../include/game_map.h"
 #include "../../include/protagonist_sprite.h"
+#include "../../include/combat_interface.h"
 
-#define PLACEHOLDER_COLOR 0xFFFFFFFF // White placeholder color
 #define MAX_PLACEHOLDERS 4
-#define HP_BAR_WIDTH 50
-#define HP_BAR_HEIGHT 5
 
 int placeholder_positions[MAX_PLACEHOLDERS][2] = {
     {452, 500},
@@ -17,21 +15,6 @@ int placeholder_positions[MAX_PLACEHOLDERS][2] = {
 };
 
 int character_hp[MAX_PLACEHOLDERS] = {100, 80, 50, 20};
-
-void draw_hp_bar(int x, int y, int hp)
-{
-    int green_width = (HP_BAR_WIDTH * hp) / 100;
-    int red_width = HP_BAR_WIDTH - green_width;
-
-    // Draw green part (current HP) on back buffer
-    drawRectARGB32_double_buffering(x, y - 10, x + green_width, y - 10 + HP_BAR_HEIGHT, 0xFF00FF00, 1);
-
-    // Draw red part (lost HP) on back buffer
-    if (red_width > 0)
-    {
-        drawRectARGB32_double_buffering(x + green_width, y - 10, x + green_width + red_width, y - 10 + HP_BAR_HEIGHT, 0xFFFF0000, 1);
-    }
-}
 
 void design_screen_loop()
 {
@@ -51,12 +34,7 @@ void design_screen_loop()
                     int pos_x = placeholder_positions[i][0];
                     int pos_y = placeholder_positions[i][1];
 
-                    drawImage_double_buffering(pos_x, pos_y, myBitmapprotag, PROTAG_WIDTH, PROTAG_HEIGHT);
-                    uart_puts("[DESIGN_SCREEN] Drawn placeholder at (");
-                    uart_dec(pos_x);
-                    uart_puts(", ");
-                    uart_dec(pos_y);
-                    uart_puts(")\n");
+                    draw_combat_character(pos_x, pos_y, myBitmapprotag, PROTAG_WIDTH, PROTAG_HEIGHT);
                 }
 
                 for (int i = 0; i < MAX_PLACEHOLDERS; ++i)
