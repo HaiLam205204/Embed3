8cb6c18b8fdd9d6205d1b92448dce912b424d04f
Vally <113294564+Needalife@users.noreply.github.com>
2025-05-16 23:26:49 +0700

    add draw_hp_bar()

--- src/game/combat_interface.c
+++ src/game/combat_interface.c
@@ -6,3 +6,23 @@
 #define HP_BAR_HEIGHT 5
 #define RED 0xFFFF0000
 #define GREEN 0xFF00FF00
+
+void draw_hp_bar(int x, int y, int hp)
+{
+    int green_width = (HP_BAR_WIDTH * hp) / 100;
+    int red_width = HP_BAR_WIDTH - green_width;
+
+    // Draw green part (current HP) on back buffer
+    drawRectARGB32_double_buffering(x, y - 10, x + green_width, y - 10 + HP_BAR_HEIGHT, GREEN, 1);
+
+    // Draw red part (lost HP) on back buffer
+    if (red_width > 0)
+    {
+        drawRectARGB32_double_buffering(x + green_width, y - 10, x + green_width + red_width, y - 10 + HP_BAR_HEIGHT, RED, 1);
+    }
+
+    uart_puts("[COMBAT_SCREEN] Drawn hp bar: ");
+    uart_dec(hp);
+    uart_puts("\n");
+}
+
