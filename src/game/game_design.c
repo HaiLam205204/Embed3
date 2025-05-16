93906f105159f112e3a6ae2c497467f35b3cdf15
Vally <113294564+Needalife@users.noreply.github.com>
2025-05-16 22:49:52 +0700

    draw hp bar

--- src/game/game_design.c
+++ src/game/game_design.c
@@ -6,6 +6,8 @@
 
 #define PLACEHOLDER_COLOR 0xFFFFFFFF // White placeholder color
 #define MAX_PLACEHOLDERS 4
+#define HP_BAR_WIDTH 50
+#define HP_BAR_HEIGHT 5
 
 int placeholder_positions[MAX_PLACEHOLDERS][2] = {
     {452, 500},
@@ -13,6 +15,23 @@ int placeholder_positions[MAX_PLACEHOLDERS][2] = {
     {664, 300},
     {472, 150},
 };
+
+int character_hp[MAX_PLACEHOLDERS] = {100, 80, 50, 20};
+
+void draw_hp_bar(int x, int y, int hp)
+{
+    int green_width = (HP_BAR_WIDTH * hp) / 100;
+    int red_width = HP_BAR_WIDTH - green_width;
+
+    // Draw green part (current HP) on back buffer
+    drawRectARGB32_double_buffering(x, y - 10, x + green_width, y - 10 + HP_BAR_HEIGHT, 0xFF00FF00, 1);
+
+    // Draw red part (lost HP) on back buffer
+    if (red_width > 0)
+    {
+        drawRectARGB32_double_buffering(x + green_width, y - 10, x + green_width + red_width, y - 10 + HP_BAR_HEIGHT, 0xFFFF0000, 1);
+    }
+}
 
 void design_screen_loop()
 {
@@ -38,6 +57,13 @@ void design_screen_loop()
                     uart_puts(", ");
                     uart_dec(pos_y);
                     uart_puts(")\n");
+                }
+
+                for (int i = 0; i < MAX_PLACEHOLDERS; ++i)
+                {
+                    int pos_x = placeholder_positions[i][0];
+                    int pos_y = placeholder_positions[i][1];
+                    draw_hp_bar(pos_x, pos_y, character_hp[i]);
                 }
 
                 swap_buffers();
