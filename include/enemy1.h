1a77cc85f3766afe91bc4dd81f1f800a4d2f2d4d
HaiLam205204 <hailam200504@gmail.com>
2025-05-17 21:44:45 +0700

    Added characters and enemies sprites

---
+++ include/enemy1.h
@@ -0,0 +1,13 @@
+#ifndef ENEMY1_SPRITE_H
+#define ENEMY1_SPRITE_H
+
+#define ENEMY1_START_X 478
+#define ENEMY1_START_Y 340
+#define ENEMY1_WIDTH 136
+#define ENEMY1_HEIGHT 88
+
+extern const unsigned long shadow1[];
+extern const int shadow1_allArray_LEN;
+extern const unsigned long* shadow1_allArray[];
+
+#endif 
