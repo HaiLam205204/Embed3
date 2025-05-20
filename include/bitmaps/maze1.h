c94eb405fe04296564799cb88b1897650dfd3cbe
HaiLam205204 <hailam200504@gmail.com>
2025-05-20 15:25:47 +0700

    1st maze done

--- include/bitmaps/maze1.h
+++ include/bitmaps/maze1.h
@@ -9,9 +9,19 @@
 #define WALL2_START_Y 64
 #define WALL2_WIDTH 63
 #define WALL2_HEIGHT 1473
+#define WALL3_START_X 0
+#define WALL3_START_Y 736
+#define WALL3_WIDTH 342
+#define WALL3_HEIGHT 63
+#define WALL4_START_X 992
+#define WALL4_START_Y 0
+#define WALL4_WIDTH 63
+#define WALL4_HEIGHT 256
 
 extern const unsigned long wall1[];
 extern const unsigned long wall2[];
+extern const unsigned long wall3[];
+extern const unsigned long wall4[];
 extern const int maze1_allArray_LEN;
 extern const unsigned long* maze1_allArray[];
 
